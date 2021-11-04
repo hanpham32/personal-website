ff23d3629d3fddbda686e754bad51575d370c071
Han Pham <hanpham@Hans-MacBook-Pro.local>
2021-11-04 00:42:04 -0700

    Updated About Me Page

--- src/pages/About.js
+++ src/pages/About.js
@@ -23,21 +23,31 @@ const About = () => {
           </div>
           <div className="flex justify-center mt-2">
             <a href="https://github.com/hanpham32" target="_blank">
-              <img src={GITHUB} alt="github-icon" className="w-8 h-8 mx-2 mb-2 transform hover:scale-110"></img>
+              <img
+                src={GITHUB}
+                alt="github-icon"
+                className="w-8 h-8 mx-2 mb-2 transform hover:scale-110"
+              ></img>
             </a>
             <a href="https://www.linkedin.com/in/hansopham/" target="_blank">
-              <img src={LINKEDIN} alt="linkedin-icon" className="w-8 h-8 mx-2 mb-2 transform hover:scale-110"></img>
+              <img
+                src={LINKEDIN}
+                alt="linkedin-icon"
+                className="w-8 h-8 mx-2 mb-2 transform hover:scale-110"
+              ></img>
             </a>
           </div>
         </div>
         <div className="lg:col-span-4">
           <p className="text-lightGrey text-xl">
-            I am a college freshman from the Seattle area. I either
-            spend my time exploring different restaurants and coffee shops in
-            Seattle, or at my computer learning various topics and creating
-            different side projects. My interests are in software development
-            with machine learning integration. In addition, I love creating
-            tools that assist human productivity and efficiency.
+            I am a college freshman from the Seattle area. I either spend my
+            time exploring different restaurants and coffee shops in Seattle, or
+            at my computer learning various topics and creating different side
+            projects. My interests are in software development with machine
+            learning integration. In addition, I love creating tools that assist
+            human productivity and efficiency. Currently, I'm researching and
+            exploring Natural Language Processing, with the main focus in Neural
+            Machine Translation.
             <br></br> Little background about me. I'm half Vietnamese and
             Chinese but I was born and grew up in Vietnam. When I was 12, I
             moved to Seattle and have lived here ever since. My other hobbies
