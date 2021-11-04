c895746b88e8f283306fce5a9e1275adc2040e36
Han Pham <hanpham@Hans-MacBook-Pro.local>
2021-11-04 00:18:57 -0700

    Fixed missing btn src

--- src/components/Project.js
+++ src/components/Project.js
@@ -12,7 +12,11 @@ const Project = ({ src, alt, desc, tags, demo, source, date }) => {
     <Container className="rounded-2xl w-11/12 h-auto bg-lightGrey2 my-4 xl:py-2 xl:px-8">
       <Row className="flex xl:grid xl:grid-cols-6">
         <div className="flex justify-center items-center lg:col-span-2">
-          <img className="rounded-t-2xl xl:rounded-2xl" src={src} alt={alt}></img>
+          <img
+            className="rounded-t-2xl xl:rounded-2xl"
+            src={src}
+            alt={alt}
+          ></img>
         </div>
         <div className="px-4 py-2 text-milkyWhite lg:col-span-3">
           <span className="flex flex-row flex-wrap">
@@ -27,15 +31,21 @@ const Project = ({ src, alt, desc, tags, demo, source, date }) => {
         </div>
         <div className="my-2 flex flex-row-reverse w-full lg:col-span-1">
           <a
+            href={demo}
+            target="_blank"
             className={
               demo == ""
                 ? "hidden"
-                : "flex justify-center items-center cursor-pointer bg-demoBtn w-16 h-8 rounded-full transform hover:scale-110 mr-2"
+                : "flex justify-center items-center cursor-pointer bg-demoBtn w-16 h-8 rounded-full transform hover:scale-110 mr-2 hover:no-underline hover:text-milkyWhite"
             }
           >
             <span>Demo</span>
           </a>
-          <a className="w-8 h-8 block mx-2 cursor-pointer transform hover:scale-110">
+          <a
+            href={source}
+            target="_blank"
+            className="w-8 h-8 block mx-2 cursor-pointer transform hover:scale-110"
+          >
             <img src={GITHUB} alt="github-icon" className="github"></img>
           </a>
         </div>
