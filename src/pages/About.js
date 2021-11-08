9cfd7829a66aab3f2d671a2cb51c7695b2e01b69
Han Pham <hanpham@Hans-MacBook-Pro.local>
2021-11-07 22:21:12 -0800

    Minor updates

--- src/pages/About.js
+++ src/pages/About.js
@@ -41,19 +41,18 @@ const About = () => {
         <div className="lg:col-span-4">
           <p className="text-lightGrey text-xl">
             I am a college freshman from the Seattle area. I either spend my
-            time exploring different restaurants and coffee shops in Seattle, or
-            at my computer learning various topics and creating different side
-            projects. My interests are in software development with machine
-            learning integration. In addition, I love creating tools that assist
-            human productivity and efficiency. Currently, I'm researching and
+            time exploring new coffee shops in Seattle, or at my computer
+            learning various topics and creating different side projects. My
+            interests are in software development, web design, and digital
+            design. I am also interested in exploring the fields of blockchain
+            technology and machine learning. Currently, I'm researching and
             exploring Natural Language Processing, with the main focus in Neural
             Machine Translation.
             <br></br> Little background about me. I'm half Vietnamese and
             Chinese but I was born and grew up in Vietnam. When I was 12, I
             moved to Seattle and have lived here ever since. My other hobbies
-            include photography and basketball. Sometimes, when the season
-            changes, I find myself go on adventures with my lovely Fujifilm
-            camera. <br></br> Thanks for giving my website a look :)
+            include photography and basketball.<br></br> Thanks for giving my
+            website a look :)
           </p>
         </div>
       </Row>
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -8,13 +8,17 @@ const Home = () => {
     <Container className="xl:flex py-8 px-4">
       <Row className="flex justify-center">
         <div className="w-4/5">
-          <h1 className="text-milkyWhite text-xl lg:text-6xl font-semibold">Hey, I'm Han</h1>
+          <h1 className="text-milkyWhite text-xl lg:text-6xl font-semibold">
+            Hey, I'm Han
+          </h1>
           <p className="text-lightGrey lg:text-2xl lg:leading-relaxed">
-            College freshman from the University of Washington
-            (Bothell). I spend my freetime to stumble on many interest-driven
-            projects. Follow my works below!
+            College freshman from the University of Washington (Bothell). I
+            spend my freetime to stumble on many interest-driven projects.
+            Follow my works below!
           </p>
-          <SocialCubes className="mt-4" />
+          <div className="xl:flex">
+            <SocialCubes className="mt-4" />
+          </div>
         </div>
       </Row>
       <Row className="flex flex-shrink-0 justify-center mt-2 xl:mr-12">
